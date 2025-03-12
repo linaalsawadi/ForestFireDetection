f46b9edae72b0cfa91d0c5e68076ef7aa2e1ebe8
lina <Lina.alsawadi2001@gmail.com>
2025-03-12 20:10:44 +0300

    Revert "IdentityLoginRegister"
    This reverts commit a3257743a60f3490acc11ebe0d64973364c00d81.

--- ForestFireDetection/Program.cs
+++ ForestFireDetection/Program.cs
@@ -1,16 +1,7 @@
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using ForestFireDetection.Data;
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("ForestFireDetectionDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ForestFireDetectionDbContextConnection' not found.");
-
-builder.Services.AddDbContext<ForestFireDetectionDbContext>(options => options.UseSqlServer(connectionString));
-
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ForestFireDetectionDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -32,6 +23,5 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
 
 app.Run();
