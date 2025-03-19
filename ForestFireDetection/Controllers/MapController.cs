c5f0ea9e2f738021f06231f92e38cc7d823a0763
lina <Lina.alsawadi2001@gmail.com>
2025-03-19 17:20:26 +0300

    editing map

--- ForestFireDetection/Controllers/MapController.cs
+++ ForestFireDetection/Controllers/MapController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ForestFireDetection.Controllers
 {
     public class MapController : Controller
     {
+        [Authorize]
         public IActionResult Index()
         {
             return View();
